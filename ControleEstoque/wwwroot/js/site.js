ab08aecf4d53689184b15042fc62d6b121f9b1fe
Rafael Vieira de Melo <rafael.melo@gmail.com>
2025-01-26 07:55:52 -0300

    Ajutes

---
+++ ControleEstoque/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿//function initializeDataTable() {
+//    $(document).ready(function () {
+//        if ($.fn.DataTable.isDataTable('#produtosTable')) {
+//            $('#produtosTable').DataTable().destroy();
+//        }
+//        $('#produtosTable').DataTable({
+//            searching: true,
+//            language: {
+//                search: "Pesquisar:", // Altera o texto da barra de pesquisa
+//                lengthMenu: "Mostrar _MENU_ registros por página",
+//                zeroRecords: "Nenhum registro encontrado",
+//                info: "Mostrando _START_ a _END_ de _TOTAL_ registros",
+//                infoEmpty: "Nenhum registro disponível",
+//                infoFiltered: "(filtrado de _MAX_ registros no total)",
+//                paginate: {
+//                    first: "Primeiro",
+//                    last: "Último",
+//                    next: "Próximo",
+//                    previous: "Anterior"
+//                }
+//            }
+//        });
+//    });
+//}
--- EstoqueApp.Infrastructure/EstoqueDbContext.cs
+++ EstoqueApp.Infrastructure/EstoqueDbContext.cs
@@ -12,26 +12,17 @@ namespace EstoqueApp.Infrastructure
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Configura o Produto
+            
             modelBuilder.Entity<Produto>()
-                .HasKey(p => p.Id);  // Definindo a chave primária
+                .HasKey(p => p.Id); 
 
             modelBuilder.Entity<Produto>()
                 .Property(p => p.Id)
-                .ValueGeneratedOnAdd();  // Garante que o Id será auto-incrementado
+                .ValueGeneratedOnAdd();
         }
-
-        // Construtor que recebe as opções de configuração, como a string de conexão
         public EstoqueDbContext(DbContextOptions<EstoqueDbContext> options)
             : base(options)
         {
         }
-
-        // Não é necessário o método OnConfiguring agora, pois a configuração vem do DI container
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    var caminhoBanco = @"C:\Users\rafael.melo\source\repos\ControleEstoque\Data\estoque.db";
-        //    optionsBuilder.UseSqlite($"Data Source={caminhoBanco}");
-        //}
     }
 }
